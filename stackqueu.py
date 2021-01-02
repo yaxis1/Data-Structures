96591717b46cdad053f2774e71a745f32e2c9773
Saifuddin Mohammad <38083799+yaxis1@users.noreply.github.com>
2021-01-02 19:06:44 +0100

    Create stackqueu.py

---
+++ stackqueu.py
@@ -0,0 +1,20 @@
+class Solution():
+  
+    def __init__(self):
+        self.stack = []
+        self.queue = [] 
+        
+
+    def pushCharacter(self,ch):
+        self.stack.append(ch)  
+    
+    def enqueueCharacter(self,ch):
+        self.queue.insert(0,ch)
+
+    def popCharacter(self):
+        return self.stack.pop() 
+    
+    def dequeueCharacter(self):
+        return self.queue[0] 
+
+
